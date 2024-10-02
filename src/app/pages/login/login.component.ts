c0f6ef425f938006e047e5da8f1475e4a3301168
th4ruka <tharukapavith2@gmail.com>
2024-10-02 21:39:52 +0530

    Add notifications to the login page.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -21,6 +21,7 @@ import { MatFormFieldModule } from "@angular/material/form-field"
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from "@angular/material/input";
 import { AuthService } from '../../core/services/auth-service/auth.service';
+import { NotificationService } from '../../core/services/notification-service/notification.service';
 
 
 @Component({
@@ -36,8 +37,9 @@ export class LoginComponent {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private authService: AuthService){
-
+    private authService: AuthService,
+    private notificationService: NotificationService
+  ){
       this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
@@ -50,15 +52,18 @@ export class LoginComponent {
 
       // Call the AuthService loginWithPassword method
       this.authService.loginWithPassword(email, password).then(user => {
-        console.log('Sign up successful:', user);
+        console.log('Login successful:', user);
+        this.notificationService.showSuccess("Login successful!");
         // Redirect to a different page (e.g., dashboard) after successful login
         this.router.navigate(['/dashboard']);
       }).catch(error => {
-        console.error('Sign up failed:', error);
+        console.error('Login failed:', error);
+        this.notificationService.showError("Error Logging in!");
         // Display error message to the user (optional: show in the template)
       });
     } else {
       console.error('Form is invalid');
+      this.notificationService.showError("Invalid data!");
       // Optionally display an error message that the form is invalid
     }
   }
@@ -66,9 +71,11 @@ export class LoginComponent {
   loginWithGoogle() {
     this.authService.loginWithGoogle().then(result => {
       console.log('Logged in successfully:', result);
+      this.notificationService.showSuccess("Login successful!");
       this.router.navigate(['/dashboard']);
     }).catch(error => {
       console.error('Login failed:', error);
+      this.notificationService.showError("Error Logging in!");
     });
   }
 }
