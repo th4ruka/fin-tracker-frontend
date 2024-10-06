b682006b4a93f492fcf3dfc07de9a7cf68d9546d
th4ruka <tharukapavith2@gmail.com>
2024-10-06 15:37:10 +0530

    Add create button component.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,16 +4,22 @@ import { RouterModule } from '@angular/router';
 
 import { NavbarService } from '../../core/services/navbar-service/navbar.service';
 
-import {MatSidenavModule, MatDrawer} from "@angular/material/sidenav";
-import {MatButtonModule} from '@angular/material/button';
-import {MatListModule} from "@angular/material/list";
+import { MatSidenavModule, MatDrawer } from "@angular/material/sidenav";
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from "@angular/material/list";
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  MatDialog,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { CreateAccountDialogComponent } from '../../core/components/create-account-dialog/create-account-dialog/create-account-dialog.component';
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatSidenavModule, MatButtonModule, MatListModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatTooltipModule, MatSidenavModule, MatButtonModule, MatListModule, MatIconModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -21,17 +27,28 @@ export class DashboardComponent implements OnInit {
   showFiller = false;
 
   //"!" says certainly drawer will be assigned a value before used.
-  @ViewChild('drawer') drawer!: MatDrawer; 
+  @ViewChild('drawer') drawer!: MatDrawer;
 
-  constructor(private navBarService: NavbarService){}
+  constructor(
+    private navBarService: NavbarService,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit() {
-    this.navBarService.toggleMenuButton$.subscribe(toggleSideNav =>{
-      if(this.drawer){
+    this.navBarService.toggleMenuButton$.subscribe(toggleSideNav => {
+      if (this.drawer) {
         this.drawer.toggle(toggleSideNav);
       }
     })
     this.navBarService.showMenuButton(true); //emits a new value to showMenuButton$ observable
+  }
+
+  openCreateAccountDialog(): void {
+    this.dialog.open(CreateAccountDialogComponent, {
+      width: '300px',
+      data: { /* pass data to the dialog here if needed */ },
+      disableClose: true
+    });
   }
 
 
